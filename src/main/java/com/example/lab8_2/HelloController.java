f12079030eeecc1227906f6528e014ef756ae68e
Tyler Ouellet <LE2171096@crc-lennox.qc.ca>
2023-09-27 09:28:22 -0400

    Added hello world button

--- src/main/java/com/example/lab8_2/HelloController.java
+++ src/main/java/com/example/lab8_2/HelloController.java
@@ -1,5 +1,6 @@
 package com.example.lab8_2;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
@@ -10,5 +11,12 @@ public class HelloController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+
+    @FXML
+    protected void onHelloWorldButtonClick(){
+        welcomeText.setText("Hello World!");
+
     }
 }
